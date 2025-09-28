4bc0cd35e9f4ad9f2c9fa5c75004d3402911d193
SpaceJumperdono <pavel-laz2010@mail.ru>
2025-09-28 15:19:26 +0300

    Инициализация проекта

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/SpaceJumperdono/java-project-72/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/SpaceJumperdono/java-project-72/actions)
+[![Quality Gate Status](https://sonarcloud.io/api/project_badges/measure?project=SpaceJumperdono_java-project-72&metric=alert_status)](https://sonarcloud.io/summary/new_code?id=SpaceJumperdono_java-project-72)
---
+++ app/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    id("java")
+    checkstyle
+    id("org.sonarqube") version "6.3.1.5724"
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "SpaceJumperdono_java-project-72")
+        property("sonar.organization", "spacejumperdono")
+    }
+}
